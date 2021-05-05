d827a93bbb6f8018008dc0375f0e020576920ef0
modernlabrat <kyrasamuel@outlook.com>
2021-05-05 02:04:05 -0400

    old work

---
+++ CMSC330/P1/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+ * Kyra Samuel
+ * CMSC330
+ * Main.java
+ * Creates an instance of Project1.
+ */
+
+public class Main {
+  public static void main(String[] args){
+      new Project1();
+    }  
+}
