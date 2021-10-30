3895014163570f9137c19457c6889ccc01928401
Kyra <kyrasamuel@outlook.com>
2021-10-29 23:33:52 -0400

    working on P1

---
+++ CMSC430/P1/Skeleton/tokens.h
@@ -0,0 +1,40 @@
+// Compiler Theory and Design
+// Dr. Duane J. Jarc
+// Modified By : Kyra Samuel
+// Date : 10 /28 / 2021
+
+// This file contains the enumerated type definition for tokens
+enum Tokens
+{
+	RELOP = 256,
+	ADDOP,
+	MULOP,
+	ANDOP,
+	BEGIN_,
+	BOOLEAN,
+	END,
+	ENDREDUCE,
+	FUNCTION,
+	INTEGER,
+	IS,
+	REDUCE,
+	RETURNS,
+	IDENTIFIER,
+	INT_LITERAL,
+	ARROW,
+	CASE,
+	ELSE,
+	ENDCASE,
+	ENDIF,
+	IF,
+	OTHERS,
+	REAL,
+	THEN,
+	WHEN,
+	OROP,
+	NOTOP,
+	EXPOP,
+	REMOP,
+	REAL_LITERAL,
+	BOOL_LITERAL
+};
